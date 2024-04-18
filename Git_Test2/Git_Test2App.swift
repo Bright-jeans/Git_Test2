10b25b4215fda0f4e8c03bdbdf15bf1a375f655b
Bright-jeans <Bright.jjeans@gmail.com>
2024-04-18 20:00:41 +0900

    Initial Commit

---
+++ Git_Test2/Git_Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Test2App.swift
+//  Git_Test2
+//
+//  Created by Leo Yoon on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
